c43c777450078ab07a8675f853741abc0bdfa4bd
KeshavDesai24 <knikhildesai22@gmail.com>
2025-07-12 12:49:49 +0000

    Fix EB environment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Developers, Welcome to NodeCloudOps!
+          Hello Developers, Welcome to NodeCloudOps.
         </p>
         <a
           className="App-link"
